7d24e903e096aead8ede679f4c3f050b9f85d878
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-22 10:08:51 -0300

    named import example

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Details from '../src/pages/Details'
+import { Details } from '../src/pages/Details'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
