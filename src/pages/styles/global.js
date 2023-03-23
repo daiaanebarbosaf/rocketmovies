05d0cdca4d4215b9cc1d03fb73e7a5fc2a3ba98e
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-23 10:37:42 -0300

    font configuration

--- src/pages/styles/global.js
+++ src/pages/styles/global.js
@@ -10,6 +10,17 @@ export default createGlobalStyle`
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
     color: ${({ theme }) => theme.COLORS.WHITE};
+
+    font-size: 62.5%;
+
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+    font-size: 1.6rem;
+
+    outline: none;
   }
 
   a {
