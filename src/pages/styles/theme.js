8328f0cae8b242377e1559b2d5b2d5743ce06e42
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-23 09:43:05 -0300

    creating project theme

---
+++ src/pages/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#262529",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#3E3B47",
+
+    PINK: "#FF859B",
+
+    GRAY_100: "#CAC4CF",
+    GRAY_300: "#948F99",
+  }
+};
