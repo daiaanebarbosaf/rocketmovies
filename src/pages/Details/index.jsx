dcdeabd6176700f5d5ac86dc4bce915775efb2bb
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-24 09:44:24 -0300

    importing the button on the main page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
 import { Container } from "./styles.js";
+import { Button } from "../../components/Button";
 
 export function Details(){
   return(
     <Container>
       <h1>Hello Word</h1>
+
+      <Button />
     </Container> 
   );
 }
