296d201c37e23a7d7d1268e26f2cf4614cd56acf
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-25 18:19:20 -0300

    styling user profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,20 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header() {
   return (
     <Container>
+
+      <Profile>
+        <div>
+            <strong>Daiane Farias</strong>
+            <span>sair</span>
+        </div>
+
+        <img 
+          src="https://github.com/daiaanebarbosaf.png" 
+          alt="Foto do usuário" 
+        />
+      </Profile>
 
     </Container>
   );
