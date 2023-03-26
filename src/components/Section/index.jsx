1362089eeb6a187f60ce860f04cd20eaf6331310
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-26 20:14:14 -0300

    Update index.jsx

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -3,7 +3,8 @@ import { Container } from "./styles";
 export function Section({ title, children }) {
   return(
     <Container>
-      <h2>{ children }</h2>
+      <h2>{title}</h2>
+      {children}
     </Container>
   );
 }
