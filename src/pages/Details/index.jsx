a55e6f97b78f13548a069ce539dbc478a569a315
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-26 20:14:57 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,18 @@
-import { Container } from "./styles.js";
+import { Container, Links } from "./styles.js";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 export function Details(){
   return(
     <Container>
       <Header />
-
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">Link 1</a></li>
+          <li>Item 02</li>
+        </Links>
+      </Section>
       <Button title="Entrar" />
     </Container> 
   );
