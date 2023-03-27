305443a7df878a0d7e719576316cc329d97928f6
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-27 11:08:25 -0300

    importing element styles

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,19 +1,34 @@
-import { Container, Links } from "./styles.js";
+import { Container, Links, Content } from "./styles.js";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details(){
   return(
     <Container>
+
       <Header />
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>
-          <li>Item 02</li>
-        </Links>
-      </Section>
-      <Button title="Entrar" />
+
+      <main>
+        <Content>
+
+        <Section title="Links úteis">
+          <Links>
+            <li><a href="#">Link 1</a></li>
+            <li>Item 02</li>
+          </Links>
+        </Section>
+
+        <Section title="Marcadores">
+          <Tag title="Ficção Científica"/>
+        </Section>
+
+        <Button title="Entrar" />
+
+      </Content>
+      </main>
+
     </Container> 
   );
 }
