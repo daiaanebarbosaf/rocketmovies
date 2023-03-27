cce5d063f205376f63e7e43250889665cfc37189
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-27 11:09:25 -0300

    Update styles.js

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.section`
     padding-bottom: 1.6rem;
     margin-bottom: 2.4rem;
 
-    color:${({ theme }) => theme.COLORS.GRAY_100};
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
     font-size: 1rem;
     font-weight: 400;
   }
