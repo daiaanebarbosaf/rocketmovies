1419b57bae894452c98f68e01098c7f2be654d78
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-29 08:35:14 -0300

    now the home is the main page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './pages/styles/global';
 
 import theme from './pages/styles/theme';
 
-import { Details } from './pages/Details';
+import { Home } from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );
