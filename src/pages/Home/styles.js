b181d49ea313ec875723dcac7aed089db852afd1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-29 08:36:38 -0300

    add style page home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: 25rem auto;
+  grid-template-rows: 105px 128px auto 64px;
+
+  grid-template-areas: 
+  "brand header"
+  "menu search"
+  "menu content"
+  "newmovie content";
+
+  background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+`;
+export const Brand = styled.div`
+  grid-area: brand;
+  background: black;
+`;
+export const Menu  = styled.ul`
+    grid-area: menu;
+    background: green;
+`;
+export const Search = styled.div`
+    grid-area: search;
+    background: violet;
+`;
+export const Content = styled.div`
+    grid-area: content;
+    background: blue;
+`;
+export const NewMovie= styled.button`
+    grid-area: newmovie;
+    background: yellow;
+`;
