786cdfb518c61e1682e674aad4644acc557df26e
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-29 08:36:21 -0300

    putting things in place on the home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,31 @@
+import { Container, Brand, Menu,  Search, Content, NewMovie } from './styles';
+
+import { Header } from '../../components/Header'
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Mymovies</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+
+      </Menu>
+
+      <Search>
+
+      </Search>
+
+      <Content>
+
+      </Content>
+
+      <NewMovie>
+        
+      </NewMovie>
+    </Container>
+  );
+}
