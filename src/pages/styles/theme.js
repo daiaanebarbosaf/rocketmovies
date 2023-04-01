977b7b46bd4b739baf6cfca40ac571bbcb2e4361
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-01 09:09:34 -0300

    add new color

--- src/pages/styles/theme.js
+++ src/pages/styles/theme.js
@@ -8,8 +8,10 @@ export default {
     WHITE_200: "#E5E5E5",
 
     PINK: "#FF859B",
+    PINK_100: "rgba(255, 133, 155, 0.05)",
 
     GRAY_100: "#CAC4CF",
+    GRAY_200: "#999591",
     GRAY_300: "#948F99",
   }
 };
