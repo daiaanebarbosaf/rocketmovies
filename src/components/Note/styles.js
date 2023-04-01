b03e07ede7323b271370a9faf66cbce474f74d2b
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-01 09:08:19 -0300

    styling the note

--- src/components/Note/styles.js
+++ src/components/Note/styles.js
@@ -2,26 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.button`
   width: 100%;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_300};
+  background-color: ${({ theme }) => theme.COLORS.PINK_100};
 
   border: none;
   border-radius: 10px;
 
-  padding: 22px;
-  margin-bottom: 16px;
+  padding: 2rem;
+  margin-bottom: 1rem;
 
   > h1 {
     flex: 1;
     text-align: left;
     font-weight: 700;
-    font-size: 24px;
 
     color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  > p {
+    text-align: left;
+    color: ${({ theme }) => theme.COLORS.GRAY_200};
+
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
   }
 
   > footer {
     width: 100%;
     display: flex;
-    margin-top: 24px;
+    margin-top: 4rem;
   }
 `;
