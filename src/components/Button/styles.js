cf12044bbcff72451b73623f2692e25bfb6157b4
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-01 18:57:03 -0300

    changing size of add movies button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-  width: 100%;
+  width: 25%;
   background-color: ${({ theme }) => theme.COLORS.PINK};
 
   color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
