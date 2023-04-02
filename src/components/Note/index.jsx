6a70ebe8de0878f658c3e523b1846f17d4687a97
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-02 08:03:00 -0300

    Update index.jsx

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -8,7 +8,7 @@ export function Note({ data, ...rest }){
       <h1>{data.title}</h1>
       <img src="../../assets/stars.svg" alt="Estrelas rosinhas " />
       <p>
-        Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; 
+      Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de
       </p>
       {
         data.tags &&
