c81f117e126368517f265dc81fbe3a0a5d069ee7
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-04 09:18:36 -0300

    add new page, the SignUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,43 @@
+import { Container, Form, Background } from './style';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+
+export function SignUp(){
+  return (
+    <Container>
+      <Form>
+        <h1>MyMovie</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Cadastrar"/>
+
+        <a href="#">Voltar para o login</a>
+      </Form>
+
+      <Background/>
+    </Container>
+  )
+}
