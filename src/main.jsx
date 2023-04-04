373595394dcfb7607a297afb4447fccfafc053b7
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-04 08:40:27 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './pages/styles/global';
 
 import theme from './pages/styles/theme';
 
-import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 );
