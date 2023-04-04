6e1d33dad7520e46a4fa7aa447478c15750eb5ea
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-04 09:19:00 -0300

    add new color

--- src/pages/styles/theme.js
+++ src/pages/styles/theme.js
@@ -1,7 +1,7 @@
 export default {
   COLORS: {
     BACKGROUND_900: "#1C1B1E",
-    BACKGROUND_800: "#312E38",
+    BACKGROUND_800: "#262529",
     BACKGROUND_700: "#3E3B47",
 
     WHITE: "#F4EDE8",
