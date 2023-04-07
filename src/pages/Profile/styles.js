341f6afaa2848ab6cd638e4c7beaec523b1a390a
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-07 11:49:13 -0300

    style of the button to change the profile image and how to put spaces between the inputs

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -29,22 +29,54 @@ export const Container = styled.div`
 
 export const Form = styled.form`
   max-width: 340px;
-  margin: 0 auto;
+  margin: 30px auto 0;
 
+  > div:nth-child(4){
+    margin-top: 24px;
+  }
 `;
 
 export const Avatar = styled.div`
   position: relative;
 
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+  margin: -124px auto 32px;
+
+  width: 186px;
+  height: 186px;
   
   > img {
     width: 186px;
     height: 186px;
 
+    border-radius: 50%;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.PINK};
 
     border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+
+    > input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
   }
 `;
