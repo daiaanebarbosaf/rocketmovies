1f200c7c6169d56ed7821271e866ae1ea388f57c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-08 17:09:29 -0300

    tweaks to place the brand inside the header

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,32 +2,14 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 
-import { Container, Brand, Content, Form } from './styles';
+import { Container, Form } from './styles';
 
 export function New() {
   return(
     <Container>
-      <Brand>
-        <h1>Mymovies</h1>
-      </Brand>
+
       <Header />
 
-      <main>
-        <Form>
-          <header>
-            <a href="/">Voltar</a>
-            <h1>Novo Filme</h1>
-          </header>
-          
-          <Content>
-            <Input 
-              placeholder="Título"
-            />
-            <Textarea placeholder="Observações"/>
-          </Content>
-
-        </Form>
-      </main>
     </Container>
   )
 }
