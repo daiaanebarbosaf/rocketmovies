3dfee09ed50dd12c9e400925ead0b50f38868849
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-08 10:58:09 -0300

    Inserting the Brand, Content and Form elements in the NEW page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,31 @@
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 
-import { Container } from './styles';
+import { Container, Brand, Content, Form } from './styles';
 
 export function New() {
   return(
     <Container>
+      <Brand>
+        <h1>Mymovies</h1>
+      </Brand>
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <a href="/">Voltar</a>
+            <h1>Novo Filme</h1>
+          </header>
+          
+          <Content>
+            <Input 
+              placeholder="Título"
+            />
+          </Content>
+
+        </Form>
+      </main>
     </Container>
   )
 }
