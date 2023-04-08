b27c4f419d85bacd77aaaae7b9378bd25c5442fa
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-08 17:28:42 -0300

    Add arrow icon

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,6 @@
+
+import { FiArrowLeft } from 'react-icons/fi';
+
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
@@ -9,6 +12,26 @@ export function New() {
     <Container>
 
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <a href="/">
+            <FiArrowLeft/>
+              Voltar
+            </a>
+            <h1>Novo filme</h1>
+          </header>
+
+          <Input 
+            placeholder="Título"
+          />
+          <Input 
+            placeholder="Sua nota (de 0 a 5)"
+          />
+          <Textarea placeholder="Observações"/>
+        </Form>
+      </main>
 
     </Container>
   )
