01433995bc65f7c6069a971a9c293729c3b95676
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-08 17:09:15 -0300

    tweaks to place the brand inside the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,36 @@
-import { Container, Profile } from './styles';
-import { FiPlus, FiSearch } from 'react-icons/fi'; 
+import { Container, Profile, Search, Brand } from './styles';
+
+import { FiSearch } from 'react-icons/fi';
+
 import { Input } from '../../components/Input';
 
 export function Header() {
   return (
     <Container>
-      <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
-      
-      <Profile>
-  
-        <div>
-            <strong>Daiane Farias</strong>
-            <span>sair</span>
-        </div>
+      <Brand>
+      <h1>MyMovies</h1>
+      </Brand>
 
+      <Search>
+        <Input
+          placeholder="Pesquisar pelo título"
+          type="text"
+          icon={FiSearch}
+          />
+      </Search>
+
+      <Profile>
+
+        <div>
+          <strong>Daiane Farias</strong>
+          <span>Sair</span>
+        </div>
         <img 
           src="https://github.com/daiaanebarbosaf.png" 
-          alt="Foto do usuário" 
+          alt="Foto do usuário(a)" 
         />
-      </Profile>
 
+      </Profile>
     </Container>
   );
 }
