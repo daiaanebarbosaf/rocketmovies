6208c0ca39b10cd1f7e1279b3929edd9b4101dd9
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-09 21:54:00 -0300

    Changing spacing between tags

--- src/components/Tag/styles.js
+++ src/components/Tag/styles.js
@@ -9,7 +9,7 @@ export const Container = styled.span`
 
   padding: 0.4rem 1rem;
   border-radius: 10px;
-  margin-right: 1rem;
+  margin-right: 10rem;
 
   color: ${({ theme }) => theme.COLORS.WHITE_200};
   background-color: ${({ theme }) => theme.COLORS.GRAY_300};
