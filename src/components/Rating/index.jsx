9333121aad99cc5179fa29dacd7f083379e39f98
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-12 10:29:59 -0300

    Create new component , the rating

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,11 @@
+import { VscStarFull, VscStarEmpty } from "react-icons/vsc";
+
+import { Container } from "./styles";
+
+export function Rating () {
+  return(
+    <Container>
+      <HiStar />
+    </Container>
+  )
+}
