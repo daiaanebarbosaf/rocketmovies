a2d076837362ca9ce6c63db17b6673f5f15ee66c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-15 17:10:56 -0300

    Adding rating to notes

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -1,12 +1,13 @@
 import { Container } from "./styles";
 
 import { Tag } from "../Tag";
+import { Rating } from "../Rating";
 
 export function Note({ data, ...rest }){
   return(
     <Container {...rest}>
       <h1>{data.title}</h1>
-      <img src="../../assets/stars.svg" alt="Estrelas rosinhas " />
+      <Rating/>
       <p>
       Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de
       </p>
