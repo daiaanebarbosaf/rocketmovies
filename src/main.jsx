6fb95684b768b630f9d08f2f25441de9e3e56f7f
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-15 17:24:16 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './pages/styles/global';
 
 import theme from './pages/styles/theme';
 
-import { New } from './pages/New';
+import { Profile } from './pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 );
