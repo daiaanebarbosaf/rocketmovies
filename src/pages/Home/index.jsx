0b63f0334f3c609df29aca573ad46c49e1ff4c18
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-15 17:01:21 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { FiPlus } from 'react-icons/fi'; 
-import { Container, Brand, NewMovie, AddMovies, Content} from './styles';
+import { Container, NewMovie, AddMovies, Content } from './styles';
 
 import { Header } from '../../components/Header';
 import { Note } from '../../components/Note';
@@ -8,9 +8,6 @@ import { Note } from '../../components/Note';
 export function Home() {
   return (
     <Container>
-      <Brand>
-        <h1>Mymovies</h1>
-      </Brand>
       <Header />
 
       <AddMovies>
@@ -22,26 +19,27 @@ export function Home() {
         </NewMovie>
       </AddMovies>
 
-      <Content>
-        <Note data={{
-              title:'Interestellar',
-              tags: [
-                {id: '1', name: 'Ficção Científica'},
-                {id: '2', name: 'Drama'}
-              ]
-            }}
-        />
+      <main>
+        <Content>
+          <Note data={{
+                title:'Interestellar',
+                tags: [
+                  {id: '1', name: 'Ficção Científica'},
+                  {id: '2', name: 'Drama'}
+                ]
+              }}
+          />
 
-        <Note data={{
-              title:'Interestellar',
-              tags: [
-                {id: '1', name: 'Ficção Científica'},
-                {id: '2', name: 'Drama'}
-              ]
-            }}
-        />
-      </Content>
-
+          <Note data={{
+                title:'Interestellar',
+                tags: [
+                  {id: '1', name: 'Ficção Científica'},
+                  {id: '2', name: 'Drama'}
+                ]
+              }}
+          />
+        </Content>
+      </main>
     </Container>
   );
 }
