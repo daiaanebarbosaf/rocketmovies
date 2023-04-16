45e7706981fc40c8ac5de7f67e95392c468c3993
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-16 08:30:46 -0300

    Changing routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from './pages/styles/global';
 
 import theme from './pages/styles/theme';
 
-import { SignUp} from './pages/SignUp';
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 );
