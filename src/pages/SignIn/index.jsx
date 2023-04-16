f79cc798f5334e8f50515236df2512ddf8b78799
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-16 08:54:32 -0300

    Linking the pages

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,6 @@
 import { Container, Form, Background } from './style';
 import { FiMail, FiLock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -28,7 +29,9 @@ export function SignIn(){
 
         <Button title="Entrar"/>
 
-        <a href="#">Criar conta</a>
+        <Link to="/register">
+          Criar conta
+        </Link>
       </Form>
 
       <Background/>
