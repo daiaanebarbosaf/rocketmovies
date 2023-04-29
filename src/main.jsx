7feccb03521ae65d476d2bc46448e6674f6a916d
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-29 18:08:24 -0300

    Criando os Hooks

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './pages/styles/global';
 
-import { MyContext } from './myContext';
+import { AuthProvider } from './hooks/auth';
 
 import theme from './pages/styles/theme';
 
@@ -13,10 +13,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MyContext.Provider value={{email: 'rodrigo@email.com'}}>
-        <Routes />
-      </MyContext.Provider>
 
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+        
     </ThemeProvider>
   </React.StrictMode>,
 );
