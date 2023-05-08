a9097d961808a5d727f70c9c6fa34c120531e6ff
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-05-08 10:12:25 -0300

    Acessando as informações do usuário pelo contexto

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -13,8 +13,8 @@ import { Button } from '../../components/Button';
 export function Profile() {
   const { user } = useAuth();
 
-  const [ name, setName ] = useState();
-  const [ email, setEmail ] = useState();
+  const [ name, setName ] = useState(user.name);
+  const [ email, setEmail ] = useState(user.email);
   const [ passwordOld, setPasswordOld ] = useState();
   const [ passwordNew, setPasswordNew ] = useState();
 
@@ -47,24 +47,30 @@ export function Profile() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input 
           placeholder="Nova Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
         <Button title="Salvar"/>
