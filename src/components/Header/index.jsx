4a76a1fe44c585208f7324dd1b71cdc3142d481e
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-05-15 09:37:36 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,19 @@
 import { Container, Profile, Search, Brand, Logout  } from './styles';
 
-import { FiSearch } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
 
-import { Input } from '../../components/Input';
-
-export function Header() {
+export function Header({}) {
   const { signOut, user } = useAuth();
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
 
   return (
     <Container>
       <Brand>
         <h1>RocketMovies</h1>
       </Brand>
-
-      <Search>
-        <Input
-          placeholder="Pesquisar pelo título"
-          type="text"
-          icon={FiSearch}
-          />
-      </Search>
 
       <Profile to="/profile">
 
