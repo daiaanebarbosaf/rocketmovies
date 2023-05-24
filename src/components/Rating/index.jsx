b639042fbc71ab1c5cd7f01b7776a398ba99a2ff
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-05-24 08:31:29 -0300

    configurando o componente para receber dados

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,14 +1,20 @@
 import { Container } from "./styles";
 import { VscStarFull, VscStarEmpty } from "react-icons/vsc";
 
-export function Rating({isActive}) {
+export function Rating({note, noteEmpty}) {
+  let noteStar = [];
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= note) {
+      noteStar.push(<VscStarFull key={i} />);
+    } else {
+      noteStar.push(<VscStarEmpty key={i} />);
+    }
+  }
+
   return(
-    <Container>
-      <VscStarFull />
-      <VscStarFull />
-      <VscStarFull />
-      <VscStarFull />
-      <VscStarEmpty />
+    <Container noteEmpty={noteEmpty}>
+      {noteStar}
     </Container>
   )
 }
