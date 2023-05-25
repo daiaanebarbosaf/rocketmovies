4e74f6be80dd850178c52b3d9c6fdf1cdd13bc01
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-05-25 08:58:50 -0300

    configurando para a imagem aparecer dinamicamente no header

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -14,6 +14,7 @@ import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
+
 export function Profile() {
   const { user, updateProfile } = useAuth();
 
