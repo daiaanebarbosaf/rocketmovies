100f5e0d7c91f4ae6a1d2ccb41885308f60d5be4
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-05-31 08:55:51 -0300

    Responsividade página de login

--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -2,54 +2,72 @@ import styled from "styled-components";
 import backgroundImg from '../../assets/bg.png';
 
 export const Container = styled.div`
+  width: 100%;
   height: 100vh;
 
   display: flex;
-  align-items: stretch;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 1024px){
+    height: 100vh;
+
+    display: flex;
+    align-items: stretch;
+  }
 
 `;
 
  export const Form = styled.form `
-  padding: 0 136px;
+  
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
+    
+    > h1 {
+      font-size: 2.7rem;
+      font-style: normal;
+      font-weight: 700;
 
-  text-align: left;
+      color:${({ theme }) => theme.COLORS.PINK};
+    }
 
+    > h2 {
+      font-size: 1.5rem;
+      margin-top: 2rem;
+      margin-bottom: 2rem;
+    }
 
-  > h1 {
-    font-size: 2.7rem;
-    font-style: normal;
-    font-weight: 700;
+    > p {
+      font-size: 0.8rem;
+      color:${({ theme }) => theme.COLORS.GRAY_100};
+    }
 
-    color:${({ theme }) => theme.COLORS.PINK};
-  }
+    > a {
+     
+      margin-top: 2rem;
+      margin-left: 6rem;
+      color:${({ theme }) => theme.COLORS.PINK};
+      
+    }
 
-  > h2 {
-    font-size: 1.5rem;
-    margin-top: 2rem;
-    margin-bottom: 2rem;
-  }
+  @media (min-width: 1024px){
+    padding: 0 136px;
+    align-items: left;
 
-  > p {
-    font-size: 0.8rem;
-    color:${({ theme }) => theme.COLORS.GRAY_100};
-  }
-
-  > a {
-    margin-top: 2rem;
-    color:${({ theme }) => theme.COLORS.PINK};
-    margin-left: 6rem;
+    text-align: left;
   }
  `;
 
 export const Background = styled.div`
-  flex: 1;
-  background: url(${backgroundImg})
-  no-repeat center center;
+  @media (min-width: 1024px){
+    flex: 1;
+    background: url(${backgroundImg})
+    no-repeat center center;
 
-  background-size: cover;
+    background-size: cover;
+  }
+
 `;
