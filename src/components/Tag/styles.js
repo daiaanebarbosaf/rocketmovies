82f271eba69779785b74acbc898dc85a09a3f48f
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-06-08 17:14:33 -0300

    Espaçamento entre as tags

--- src/components/Tag/styles.js
+++ src/components/Tag/styles.js
@@ -9,6 +9,7 @@ export const Container = styled.span`
 
   padding: 0.4rem 1rem;
   border-radius: 10px;
+  margin-right: 0.5rem;
 
   color: ${({ theme }) => theme.COLORS.WHITE_200};
   background-color: ${({ theme }) => theme.COLORS.PINK_100};
