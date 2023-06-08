22d01f5cdbdfa1460d4d88ac189815a8d7ae2c6c
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-06-08 17:14:19 -0300

    padronizando o alinhamento na página e não no componente

--- src/components/Rating/styles.js
+++ src/components/Rating/styles.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
-  display: flex ;
-  align-items: center;
+
   > svg {
     background: transparent;
     color: ${({ theme }) => theme.COLORS.PINK};
